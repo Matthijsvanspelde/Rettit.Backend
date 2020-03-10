0df9fa99d11a7aaf1054bf19f3366d322a8c69a7
Matthijs van Spelde <I404906@fhict.nl>
2020-03-10 11:24:57 +0100

    Initial commit

---
+++ Rettit.DAL/UserContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rettit.Models;
+
+namespace Rettit.DAL
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> User { get; set; }
+    }
+}
